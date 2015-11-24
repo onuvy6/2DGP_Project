c513c622189b07701ae4c6b9011dc7f5cb6b8824
이승철 <onuvy6@gmail.com>
2015-11-24 17:16:05 +0900

    Terrorlight 캐릭터 추가 및 TileState와 GameState 추가

--- 2DGPProject/title_state.py
+++ 2DGPProject/title_state.py
@@ -1,11 +1,12 @@
 ﻿import game_framework
-import game_state
+import game_state_level_01
 import map_loader
 import collision
 
 import character_data
 import finn_character
 import cubchoo_character
+import terrorlight_character
 
 from pico2d import *
 from pico2d_extension import *
@@ -18,13 +19,13 @@ def enter():
     image = load_image('Resources/States/Background_01.png')
 
     global map
-    map = map_loader.load_map('Resources/Maps/Level_02.json')
-
-    global finn
-    finn = finn_character.Finn()
+    map = map_loader.load_map('Resources/Maps/Title.json')
 
     global cubchooes
-    cubchooes = [cubchoo_character.Cubchoo() for i in range(10)]
+    cubchooes = [cubchoo_character.Cubchoo() for i in range(3)]
+
+    global terrorlights
+    terrorlights = [terrorlight_character.Terrorlight() for i in range(3)]
 
 
 def exit():
@@ -32,19 +33,19 @@ def exit():
     
     del (map)
 
-    del (finn)
     del (cubchooes)
 
 
 def update():
 
-    finn.update()
-    collision.collision_map_and_character(map, finn)
-
     for cubchoo in cubchooes:
         cubchoo.update()
         collision.collision_map_and_character(map, cubchoo)        
     
+    for terrorlight in terrorlights:
+        terrorlight.update()
+        collision.collision_map_and_character(map, terrorlight) 
+
 
 def draw():
     clear_canvas()
@@ -52,12 +53,12 @@ def draw():
     image.draw(game_framework.width//2, game_framework.height//2)
 
     map.draw_low()
-    map.draw_hexagon_on_point(finn.x, finn.y)
-
-    finn.draw()
-
+    
     for cubchoo in cubchooes:
         cubchoo.draw()
+
+    for terrorlight in terrorlights:
+        terrorlight.draw()
 
     map.draw_high()
 
@@ -67,14 +68,7 @@ def draw():
 def handle_events():
     events = get_events()
     for event in events:
-        if event.type == SDL_KEYDOWN:
-            state = finn.get_state_from_key(event.key)
-            if state is not None:
-                finn.change_state(state)
-
-        elif event.type == SDL_KEYUP:
-            if event.key == finn.get_key_from_state():
-                finn.frame_stop = True
+        pass
 
 
 def pause():
